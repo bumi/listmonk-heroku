86f947ba807cbf73ab681468aea28f290a2ae064
Michael Bumann <hello@michaelbumann.com>
2020-10-26 19:17:10 +0100

    Adjust for new listmonk 0.7 version
    Most settings can now be configured through the UI.
    And listmonk has authentication built in.

--- start-listmonk.sh
+++ start-listmonk.sh
@@ -4,10 +4,6 @@ if [ ! -f config/listmonk.toml ]; then
   echo "Compiling listmonk config file"
   erb config/listmonk.toml.erb > config/listmonk.toml
 fi
-if [ ! -f config/htpasswd ]; then
-  echo "Writing basic auth file"
-  printf "$APP_USERNAME:$(openssl passwd -crypt $APP_PASSWORD)\n" >> config/htpasswd
-fi
 
 touch /tmp/app-initialized
 ./listmonk --config config/listmonk.toml
